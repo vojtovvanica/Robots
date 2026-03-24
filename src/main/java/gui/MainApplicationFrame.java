784875a9df4bf65bee91127491510a207054a33c
nika <vojtovanika07@gmail.com>
2026-03-24 13:23:42 +0500

    addWindowListener

--- src/main/java/gui/MainApplicationFrame.java
+++ src/main/java/gui/MainApplicationFrame.java
@@ -5,6 +5,7 @@ import log.Logger;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.KeyEvent;
+import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 /**
  * Что требуется сделать:
@@ -16,33 +17,42 @@ import java.awt.event.WindowEvent;
 public class MainApplicationFrame extends JFrame
 {
     private final JDesktopPane desktopPane = new JDesktopPane();
-    
+
     public MainApplicationFrame() {
-        //Make the big window be indented 50 pixels from each edge
-        //of the screen.
-        int inset = 50;        
+        int inset = 50;
         Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
         setBounds(inset, inset,
-            screenSize.width  - inset*2,
-            screenSize.height - inset*2);
+                screenSize.width  - inset*2,
+                screenSize.height - inset*2);
 
         setContentPane(desktopPane);
-        
-        
+
         LogWindow logWindow = createLogWindow();
         addWindow(logWindow);
 
         GameWindow gameWindow = new GameWindow();
-        gameWindow.setSize(400,  400);
+        gameWindow.setSize(400, 400);
         addWindow(gameWindow);
 
         MenuButton menuButton = new MenuButton(this);
         setJMenuBar(menuButton.generateMenuBar());
-
-
         setDefaultCloseOperation(DO_NOTHING_ON_CLOSE);
+        addWindowListener(new WindowAdapter() {
+            @Override
+            public void windowClosing(WindowEvent event) {
+                closeApplication();
+            }
+        });
     }
-    
+    public void closeApplication() {
+        int message = JOptionPane.showConfirmDialog(this, "Вы действительно выходите?", "Подтверждение ",
+                JOptionPane.YES_NO_OPTION, JOptionPane.QUESTION_MESSAGE);
+        if (message == JOptionPane.YES_OPTION) {
+            this.dispose();
+            System.exit(0);
+        }
+    }
+
     protected LogWindow createLogWindow()
     {
         LogWindow logWindow = new LogWindow(Logger.getDefaultLogSource());
@@ -53,7 +63,7 @@ public class MainApplicationFrame extends JFrame
         Logger.debug("Протокол работает");
         return logWindow;
     }
-    
+
     protected void addWindow(JInternalFrame frame)
     {
         desktopPane.add(frame);
--- src/main/java/gui/MenuButton.java
+++ src/main/java/gui/MenuButton.java
@@ -10,12 +10,9 @@ import java.awt.*;
 import java.awt.event.KeyEvent;
 import java.awt.event.WindowEvent;
 
+
 public class MenuButton {
     private final JFrame parentFrame;
-    /** Создает панель меню
-     * Для добавления новых пунктов меню используется add
-     * @return готовая панель меню JMenuBar
-     * */
     public MenuButton(JFrame parentFrame){
         this.parentFrame = parentFrame;
     }
@@ -27,11 +24,6 @@ public class MenuButton {
         menuBar.add(closeMenu());
         return menuBar;
     }
-    /** Реализует пункт меню, отвечающий за отображение
-     * С помощью метода setMnemonic и константы KeyEvent.VK_V обнаруживаем нажатие клавиш
-     * С помощью метода add добавляем подпункты
-     * @return меню выбора режима отображения
-     * */
     private JMenu lookAndFeelMenu() {
         JMenu lookAndFeelMenu = new JMenu("Режим отображения");
         lookAndFeelMenu.setMnemonic(KeyEvent.VK_V);
@@ -41,12 +33,6 @@ public class MenuButton {
         lookAndFeelMenu.add(crossplatformLookAndFeel());
         return lookAndFeelMenu;
     }
-    /** Реализация подпункта lookAndFeelMenu
-     * Используем обработчик событий addActionListener, в котором с помощью
-     * лямбда-выражения вызывается метод setLookAndFeel
-     * getSystemLookAndFeelClassName - реализует системный внешний вид
-     * @return пункт меню для установки системной схемы
-     * */
     private JMenuItem systemLookAndFeel(){
         JMenuItem systemLookAndFeel = new JMenuItem("Системная схема", KeyEvent.VK_S);
         systemLookAndFeel.addActionListener((event) -> {
@@ -54,12 +40,6 @@ public class MenuButton {
         });
         return systemLookAndFeel;
     }
-    /** Реализация подпункта lookAndFeelMenu
-     * Используем обработчик событий addActionListener, в котором с помощью
-     * лямбда-выражения вызывается метод setLookAndFeel
-     * getCrossPlatformLookAndFeelClassName - реализует универсальный внешний вид
-     * @return пункт меню для установки универсальной схемы
-     * */
     private JMenuItem crossplatformLookAndFeel() {
         JMenuItem crossplatformLookAndFeel = new JMenuItem("Универсальная схема", KeyEvent.VK_S);
         crossplatformLookAndFeel.addActionListener((event) -> {
@@ -80,10 +60,7 @@ public class MenuButton {
             // just ignore
         }
     }
-    /** Создает меню для тестовых команд
-     * С помощью setMnemonic и KeyEvent.VK_T устанавливаем быстрый доступ
-     * @return меню "Тесты"
-     * */
+
     private JMenu testMenu(){
         JMenu testMenu = new JMenu("Тесты");
         testMenu.setMnemonic(KeyEvent.VK_T);
@@ -92,10 +69,6 @@ public class MenuButton {
         testMenu.add(addLogMessageItem());
         return testMenu;
     }
-    /** Реализует добавление сообщения в лог
-     * Используем Logger.debug для записи в лог
-     * @return пункт меню "Сообщение в лог"
-     * */
 
     private JMenuItem addLogMessageItem(){
         JMenuItem addLogMessageItem = new JMenuItem("Сообщение в лог", KeyEvent.VK_S);
@@ -104,33 +77,18 @@ public class MenuButton {
         });
         return addLogMessageItem;
     }
-    /** Создает меню для закрытия приложения
-     * @return меню "Закрыть"
-     * */
     private JMenu closeMenu(){
         JMenu closeMenu = new JMenu("Закрыть");
         closeMenu.setMnemonic(KeyEvent.VK_V);
         closeMenu.add(questionClose());
         return closeMenu;
     }
-    /** Реализует сборку и обработку события выхода из приложения
-     * Используя класс JOptionPane, создается диалоговое окно
-     * YES_NO_OPTION - реализует кнопки диалога да/нет
-     * setDefaultCloseOperation - пользовательское закрытие через крестик
-     * dispatchEvent - искусственное закрытие
-     * */
-    public void closeApplication(){
-        int message = JOptionPane.showConfirmDialog(parentFrame, "Вы действительно выходите?", "Подтверждение ",
-                JOptionPane.YES_NO_OPTION, JOptionPane.QUESTION_MESSAGE);
-        if (message == JOptionPane.YES_OPTION){
-            parentFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-            parentFrame.dispatchEvent(new WindowEvent(parentFrame, WindowEvent.WINDOW_CLOSING));
-        }
-    }
 
     private JMenuItem questionClose(){
         JMenuItem checkCloseMenu = new JMenuItem("Вы уверены?", KeyEvent.VK_S);
-        checkCloseMenu.addActionListener((event)-> {closeApplication();});
+        checkCloseMenu.addActionListener((event)-> {
+            ((MainApplicationFrame) parentFrame).closeApplication();
+        });
         return checkCloseMenu;
     }
 
